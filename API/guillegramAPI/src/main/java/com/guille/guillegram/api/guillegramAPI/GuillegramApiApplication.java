24723bb54a3ffa24b0e32a48e8c7a0aadcb1791e
ekzGuille <guillermosesesantos@hotmail.com>
2019-02-24 11:15:16 +0100

    Web service API creado

---
+++ API/guillegramAPI/src/main/java/com/guille/guillegram/api/guillegramAPI/GuillegramApiApplication.java
@@ -0,0 +1,13 @@
+package com.guille.guillegram.api.guillegramAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuillegramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuillegramApiApplication.class, args);
+	}
+
+}
