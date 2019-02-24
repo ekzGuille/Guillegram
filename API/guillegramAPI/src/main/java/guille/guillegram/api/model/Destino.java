d5baa081a63150f651d45f71a6f19a0fca1058b8
ekzGuille <guillermosesesantos@hotmail.com>
2019-02-24 13:06:16 +0100

    Modelos creados

---
+++ API/guillegramAPI/src/main/java/guille/guillegram/api/model/Destino.java
@@ -0,0 +1,83 @@
+package guille.guillegram.api.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Destino {
+
+	@Id
+	@Column(nullable = false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int id;
+
+	@Column(nullable = false)
+	private String descripcion;
+
+	@Column(nullable = false)
+	private String imagen;
+
+	@Column(nullable = false)
+	private double latitud;
+
+	@Column(nullable = false)
+	private double longitud;
+
+	@ManyToMany(targetEntity = Usuario.class)
+	private Set<?> listaUsuarios;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public double getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(double latitud) {
+		this.latitud = latitud;
+	}
+
+	public double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(double longitud) {
+		this.longitud = longitud;
+	}
+
+	public Set<?> getListaUsuarios() {
+		return listaUsuarios;
+	}
+
+	public void setListaUsuarios(Set<?> listaUsuarios) {
+		this.listaUsuarios = listaUsuarios;
+	}
+
+}
---
+++ API/guillegramAPI/src/main/java/guille/guillegram/api/model/Usuario.java
@@ -0,0 +1,83 @@
+package guille.guillegram.api.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@Column(nullable = false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int id;
+
+	@Column(nullable = false)
+	private String nombre;
+
+	@Column(nullable = false)
+	private String correo;
+
+	@Column(nullable = false)
+	private String nombreUsuario;
+
+	@Column(nullable = false)
+	private String contrasena;
+
+	@ManyToMany(targetEntity = Destino.class)
+	private Set<?> listaDestinos;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public Set<?> getListaDestinos() {
+		return listaDestinos;
+	}
+
+	public void setListaDestinos(Set<?> listaDestinos) {
+		this.listaDestinos = listaDestinos;
+	}
+
+}
